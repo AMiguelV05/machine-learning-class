7b21c9c04299996b95187046ea80cad9caa6f51a
Angel Villallvazo <angel.villalvazo5367@alumnos.udg.mx>
2026-02-05 19:52:45 -0600

    Rename some variables and functions. Added three different charts, one with the original data, another with the train-test ratio and the last one with the plane over the original data.

--- linear-regression/multiple-linear-regression.py
+++ linear-regression/multiple-linear-regression.py
@@ -6,25 +6,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, root_mean_squared_error
 
 
-def mainfunc():
+def create_multiple_lr_regression():
+    # Declaring the given data
     data = {
-        'horas': [5, 20, 5, 15, 10, 8, 12, 18, 6, 14, 9, 16, 7, 11, 13, 4,
+        'hours': [5, 20, 5, 15, 10, 8, 12, 18, 6, 14, 9, 16, 7, 11, 13, 4,
                   17, 19, 3, 10, 6, 14, 8, 12, 15, 7, 9, 11, 13, 16],
-        'tareas': [8, 2, 9, 11, 10, 7, 10, 3, 8, 12, 9, 5, 6, 11, 8, 10, 4,
+        'homework': [8, 2, 9, 11, 10, 7, 10, 3, 8, 12, 9, 5, 6, 11, 8, 10, 4,
                    1, 12, 7, 9, 6, 8, 10, 5, 11, 7, 9, 12, 4],
-        'calificacion': [10, 30, 20, 25, 15, 18, 22, 28, 12, 24, 17, 26, 14,
+        'grade': [10, 30, 20, 25, 15, 18, 22, 28, 12, 24, 17, 26, 14,
                          23, 21, 8, 27, 32, 6, 19, 13, 25, 16, 20, 29, 15, 18, 12, 24, 31]
     }
+    # Create a pandas data frame with the dictionary
     df = pd.DataFrame(data)
-    X = df[['horas', 'tareas']]
-    y = df['calificacion']
+    X = df[['hours', 'homework']]
+    y = df['grade']
 
-    trainX, testX, trainY, testY = train_test_split(X, y,
-                                                    test_size=0.2, random_state=42)
+    # Splitting the data into 80% training and 20% testing
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.2, random_state=42)
     model = LinearRegression()
-    model.fit(trainX, trainY)
+    model.fit(X_train, y_train)
 
-    prediction = model.predict(testX)
+    prediction = model.predict(X_test)
     print("\n----MODEL PARAMETERS----")
     print(f"θ₀ (Intersection): {model.intercept_:.5f}")
     print(f"θ₁ (Study weight): {model.coef_[0]:.5f}")
@@ -32,22 +35,62 @@ def mainfunc():
     print("\n----RESULTANT EQUATION----")
     print(f"Equation: y = {model.intercept_:.5f} + {model.coef_[0]:.5f}(X₁) + {model.coef_[1]:.5f}(X₂)")
     print("\n----EVALUATION METRICS----")
-    print(f"MSE: {mean_squared_error(testY, prediction):.5f}")
-    print(f"RMSE: {root_mean_squared_error(testY, prediction):.5f}")
-    print(f"R²: {r2_score(testY, prediction):.5f}")
-    create_charts(df)
+    print(f"MSE: {mean_squared_error(y_test, prediction):.5f}")
+    print(f"RMSE: {root_mean_squared_error(y_test, prediction):.5f}")
+    print(f"R²: {r2_score(y_test, prediction):.5f}")
+    create_charts(df, X_train, y_train, X_test, y_test, model)
 
-def create_charts(df):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    xs = df['horas']
-    ys = df['tareas']
-    zs = df['calificacion']
 
-    ax.scatter(xs, ys, zs=zs, marker='o')
-    ax.set_xlabel('horas')
-    ax.set_ylabel('tareas')
-    ax.set_zlabel('calificacion')
+def create_table(df):
+    pass
+
+
+def set_axis_labels(ax):
+    ax.set_xlabel('hours')
+    ax.set_ylabel('homework')
+    ax.set_zlabel('grade')
+
+
+def create_charts(df, X_train, y_train, X_test, y_test, model):
+    # 3D Chart showing the original data
+    ax1 = plt.figure().add_subplot(projection='3d')
+
+    ax1.scatter(df['hours'], df['homework'], zs=df['grade'], marker='o')
+    set_axis_labels(ax1)
+
+    plt.title("Original data")
     plt.show()
 
-mainfunc()
+    # 3D Chart showing the 80-20 proportion
+    ax2 = plt.figure().add_subplot(projection='3d')
+
+    ax2.scatter(X_train['hours'], X_train['homework'], zs=y_train, marker='o', c='green', label='Train data')
+    ax2.scatter(X_test['hours'], X_test['homework'], zs=y_test, marker='*', c='orange', label='Test data')
+    ax2.legend()
+    set_axis_labels(ax2)
+
+    plt.title("Train data vs Test data")
+    plt.show()
+
+    # 3D chart showing the plane over the original data
+    ax3 = plt.figure().add_subplot(projection='3d')
+
+    ax3.scatter(df['hours'], df['homework'], zs=df['grade'], marker='o')
+
+    # Making our data smoother for a better plane
+    X_mesh = np.linspace(min(df['hours']), max(df['hours']), 20)
+    Y_mesh = np.linspace(min(df['homework']), max(df['homework']), 20)
+    
+    # Creates a tuple of coordinate matrices
+    X_mesh, Y_mesh = np.meshgrid(X_mesh, Y_mesh)
+    
+    # We calculate the dependent variable for each data in the 20*20 matrices
+    Z_mesh = model.intercept_ + model.coef_[0] * X_mesh + model.coef_[1] * Y_mesh
+
+    ax3.plot_surface(X_mesh, Y_mesh, Z_mesh, color='green', alpha=0.4)
+    set_axis_labels(ax3)
+
+    plt.show()
+
+if __name__ == "__main__":
+    create_multiple_lr_regression()
