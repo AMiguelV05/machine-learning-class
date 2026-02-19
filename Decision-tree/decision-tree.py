03ee4e0e08f100824a075a3d57c13506ba99bf94
Angel Villallvazo <angel.villalvazo5367@alumnos.udg.mx>
2026-02-19 15:55:48 -0600

    Add two decision tree models: One with max_depth=4 and min_samples_split=3 One without depth or split restrictions

---
+++ Decision-tree/decision-tree.py
@@ -0,0 +1,76 @@
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def training_model():
+    # Reading the data from the archive
+    df = pd.read_csv('titanic_datos.csv')
+    # We processed the data
+    X, y = process_data(df)
+
+    # Train split with a 70(train)-30(test) ratio
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+    # We create and train our model with overfitting
+    dt = DecisionTreeClassifier()
+    dt.fit(X_train, y_train)
+
+    # Create a prediction for the overfitting tree over our X_test
+    y_pred = dt.predict(X_test)
+
+    # Calculate the metrics and print 'em in console
+    print("\nMetrics for tree with overfitting:\n")
+    calculate_metrics(y_test, y_pred)
+    create_tree(dt, complementTitle='without pruning', X=X)
+
+    # We create and train our model with pre-pruning
+    dt2 = DecisionTreeClassifier(max_depth=4, min_samples_split=3)
+    dt2.fit(X_train, y_train)
+
+    # Create a prediction for the pre-pruning tree over our X_test
+    y_pred2 = dt2.predict(X_test)
+
+    # Calculate the metrics and print 'em in console
+    print("\nMetrics for tree with pre-pruning:\n")
+    calculate_metrics(y_test, y_pred2)
+    create_tree(dt2, complementTitle='with pre-pruning', X=X)
+
+
+def process_data(df):
+    # Setting number values for attribute sex: male(1) and female(0)
+    df['Sex'] = df['Sex'].map({'male': 1, 'female': 0})
+
+    # All the attributes
+    X = df.drop('Survived', axis=1)
+    # Target class
+    y = df['Survived']
+
+    return X, y
+
+
+def calculate_metrics(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    print("---Metrics---")
+    print(f"Accuracy: {accuracy:4f}")
+    print(f"F1 Score: {f1:4f}")
+    print(f"Precision: {precision:4f}")
+    print(f"Recall: {recall:4f}")
+
+
+def create_tree(dt, complementTitle: str, X):
+    plot_tree(
+        dt,
+        feature_names=X.columns,
+        class_names=['Survived', 'Not survided'],
+        filled=True)
+    plt.title(f'Decision tree classifier {complementTitle}')
+    plt.show()
+
+if __name__ == '__main__':
+    training_model()
