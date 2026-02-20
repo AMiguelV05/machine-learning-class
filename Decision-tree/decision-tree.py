9a9bd69d960690b7eafa59b664c0ebd351a3db95
Angel Villallvazo <angel.villalvazo5367@alumnos.udg.mx>
2026-02-19 23:32:05 -0600

    Added new metrics and a table: Confusion matrix and specificity were added. Function create_table was added to visualize the difference between predictions and real values.

--- Decision-tree/decision-tree.py
+++ Decision-tree/decision-tree.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from sklearn.metrics import (accuracy_score, f1_score, recall_score, precision_score,
+                             confusion_matrix, ConfusionMatrixDisplay)
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -10,7 +11,6 @@ def training_model():
     df = pd.read_csv('titanic_datos.csv')
     # We processed the data
     X, y = process_data(df)
-
     # Train split with a 70(train)-30(test) ratio
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
@@ -23,7 +23,7 @@ def training_model():
 
     # Calculate the metrics and print 'em in console
     print("\nMetrics for tree with overfitting:\n")
-    calculate_metrics(y_test, y_pred)
+    calculate_metrics(y_test, y_pred, complementTitle='with overfitting')
     create_tree(dt, complementTitle='without pruning', X=X)
 
     # We create and train our model with pre-pruning
@@ -34,9 +34,10 @@ def training_model():
     y_pred2 = dt2.predict(X_test)
 
     # Calculate the metrics and print 'em in console
-    print("\nMetrics for tree with pre-pruning:\n")
-    calculate_metrics(y_test, y_pred2)
+    print("Metrics for tree with pre-pruning:\n")
+    calculate_metrics(y_test, y_pred2, complementTitle='with pre-pruning')
     create_tree(dt2, complementTitle='with pre-pruning', X=X)
+    create_table(df, dt, dt2, X)
 
 
 def process_data(df):
@@ -51,23 +52,49 @@ def process_data(df):
     return X, y
 
 
-def calculate_metrics(y_true, y_pred):
+def calculate_metrics(y_true, y_pred, complementTitle: str):
+    # Calculating the different metrics
     accuracy = accuracy_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
     precision = precision_score(y_true, y_pred)
     recall = recall_score(y_true, y_pred)
     print("---Metrics---")
-    print(f"Accuracy: {accuracy:4f}")
-    print(f"F1 Score: {f1:4f}")
-    print(f"Precision: {precision:4f}")
-    print(f"Recall: {recall:4f}")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    # We calculate the confusion matrix
+    confusionMatrix = confusion_matrix(y_true, y_pred)
+    # Obtaining the values from the matrix
+    tn, fp, fn, tp = confusionMatrix.ravel()
+    # Calculating specificity
+    specificity = tn / (tn + fp)
+    print(f"Specificity: {specificity:.4f}\n")
+    # Using the function to display the confusion matrix
+    ConfusionMatrixDisplay(confusionMatrix, display_labels=['Not survived', 'Survived']).plot()
+    plt.title(f'Confusion Matrix {complementTitle}')
+    plt.show()
+
+
+def create_table(df: pd.DataFrame, dt, dt2, X):
+    # We recalculate the predictions with the entire dataset
+    y_pred1 = dt.predict(X)
+    y_pred2 = dt2.predict(X)
+    # Creating two new columns
+    df = df.assign(predicted1=y_pred1)
+    df = df.assign(predicted2=y_pred2)
+    # Re-naming columns
+    df.columns = ['ID', 'Class', 'Sex', 'Age', 'SibSp', 'Fare', 'Real survived',
+                  'Predicted overfitting', 'Predicted pre-pruning']
+    print(df.to_string(index=False, justify='center'))
 
 
 def create_tree(dt, complementTitle: str, X):
+    # We use the "plot_tree" function to display it in a chart
     plot_tree(
         dt,
         feature_names=X.columns,
-        class_names=['Survived', 'Not survided'],
+        class_names=['Not survived', 'Survived'],
         filled=True)
     plt.title(f'Decision tree classifier {complementTitle}')
     plt.show()
