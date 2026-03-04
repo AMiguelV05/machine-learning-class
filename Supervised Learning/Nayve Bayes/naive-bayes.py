01b85946785cc52ef1113605c398d75ffd9f7121
Angel Villalvazo <angel.villalvazo5367@alumnos.udg.mx>
2026-03-04 17:52:56 -0600

    Implement performance metrics for the algorithm - Add a new function to the pipeline (evaluate). - Integrate confusion_matrix and ConfusionMatrixDisplay to easily notice the performance. - Add metrics such as: accuracy, f1 score, precision, recall and specificity.

--- Supervised Learning/Nayve Bayes/naive-bayes.py
+++ Supervised Learning/Nayve Bayes/naive-bayes.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import (accuracy_score, f1_score, recall_score, precision_score,
                              confusion_matrix, ConfusionMatrixDisplay)
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 def load_data():
@@ -36,12 +37,34 @@ def create_print_dataframe(X_test, y_test, y_pred):
     print(df.to_string(index=False, justify='center', col_space=20))
 
 
+def evaluate_model(y_test, y_pred):
+    positiveClass = 'spam'
+    negativeClass = 'no spam'
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, pos_label=positiveClass)
+    precision = precision_score(y_test, y_pred, pos_label=positiveClass)
+    recall = recall_score(y_test, y_pred, pos_label=positiveClass)
+    confusionMatrix = confusion_matrix(y_test, y_pred, labels=[negativeClass, positiveClass])
+    tn, fp, fn, tp = confusionMatrix.ravel()
+    specificity = tn / (tn + fp)
+    print("---Metrics---")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"Specificity: {specificity:.4f}\n")
+    ConfusionMatrixDisplay(confusionMatrix, display_labels=['Not Spam', 'Spam']).plot()
+    plt.title("Confusion Matrix")
+    plt.show()
+
+
 def main():
     X, y = load_data()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=450)
     model, X_test_v = model_training(X_train, X_test, y_train)
     y_pred = model_prediction(model, X_test_v)
     create_print_dataframe(X_test, y_test, y_pred)
+    evaluate_model(y_test, y_pred)
 
 
 if __name__ == '__main__':
