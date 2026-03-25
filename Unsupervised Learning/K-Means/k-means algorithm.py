b60c9200a89db1ede9edabd8d4835e7cbb930df5
Angel Villalvazo <angel.villalvazo5367@alumnos.udg.mx>
2026-03-24 18:07:23 -0600

    Implement K-means algorithm in its entirely: -Add a function to recalculate the centroids after an update if there are no points near the centroid it chooses a new centroid. -Add a function that loops the main logic to obtain the best centroids, it stops until there are no changes or when we reach the 100th iteration. - Add a function to display the entire dataset and the centroids in 2D.

--- Unsupervised Learning/K-Means/k-means algorithm.py
+++ Unsupervised Learning/K-Means/k-means algorithm.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 
-def load_data(path='IRIS_PLANT_NO_CLASSES.csv'):
-    X = pd.read_csv(path)
+def load_data():
+    X = pd.read_csv(Path(__file__).parent / 'IRIS_PLANT_NO_CLASSES.csv')
     return X
 
 
@@ -22,16 +24,56 @@ def single_euclidean_distance(X: pd.DataFrame, centroid):
     return distances
 
 
+def update_centroids(X: pd.DataFrame, clusters: np.ndarray, k):
+    newCentroids = []
+    XNumPy = X.to_numpy()
+    print("\nSe actualizaron los centroides\n")
+    for i in range(k):
+        pointsInCluster = XNumPy[clusters == i]
+
+        if len(pointsInCluster) > 0:
+            newCentroids.append(np.mean(pointsInCluster, axis=0))
+        else:
+            newCentroids.append(X.sample(n=1).to_numpy()[0])
+            print("Se tuvo que crear uno nuevo")
+
+    return np.array(newCentroids)
+
+
+def cluster_definition(X, centroids):
+    centroidUpdatedTimes = 0
+    while True or centroidUpdatedTimes < 100:
+        distances = []
+        for centroid in centroids:
+            distances.append(single_euclidean_distance(X, centroid))
+        distances = np.array(distances)
+        winningIndexes = np.argmin(distances, axis=0)
+        previous_centroids = centroids
+        centroids = update_centroids(X, winningIndexes, 3)
+        centroidUpdatedTimes += 1
+        print(centroidUpdatedTimes)
+        if np.array_equal(previous_centroids, centroids):
+            break
+    return centroids
+
+
+def create_charts(X: pd.DataFrame, finalCentroids):
+    plt.scatter(X['sepal_length'], X['sepal_width'], color='#00DDFF', label='Entire dataset')
+    plt.scatter(finalCentroids[:, 0], finalCentroids[:, 1], color='#F700FF', label='Final centroids')
+    plt.xlabel("Sepal Length")
+    plt.ylabel("Sepal Width")
+    plt.title("Iris plant clusters")
+    plt.legend()
+    plt.show()
+
+
 def main():
     X = load_data()
     centroids = select_centroids(X, 3)
-    distances = []
-    for centroid in centroids:
-        distances.append(single_euclidean_distance(X, centroid))
-    distances = np.array(distances)
-    print(X)
-    print(np.argmin(distances, axis=0))
+    finalCentroids = cluster_definition(X, centroids)
+
+    create_charts(X, finalCentroids)
 
 
-
-main()
+if __name__ == "__main__":
+    main()
